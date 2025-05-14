36b4490a57befda899df73a8b7630e13d98faaa1
Dorindeacuu <accounts@salby.net>
2025-05-14 15:35:19 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/', // Ensure this is set for deploying to username.github.io
+  base: "/Dorindeacuu.github.io/", 
   server: {
     host: "::",
     port: 8080,
